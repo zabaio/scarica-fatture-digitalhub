e27c22612559e44f99787760d9a4de7627e73e6e
Alberto Giusti <albertogiusti.giusti@gmail.com>
2026-02-07 00:53:47 +0100

    test: workflow

--- config/config-validate.js
+++ config/config-validate.js
@@ -1,6 +1,8 @@
 var validate = require('jsonschema').validate;
 var fs = require('fs');
-var json = JSON.parse(fs.readFileSync("config\\config.json", "utf-8"));
-var schema = JSON.parse(fs.readFileSync("config\\config.schema.json", "utf-8"));
+const configPath = path.join('config', 'config.json'); 
+const schemaPath = path.join("config","config.schema.json","utf-8");
+var json = JSON.parse(fs.readFileSync(configPath, "utf-8"));
+var schema = JSON.parse(fs.readFileSync(schemaPath, "utf-8"));
 var res = validate(json, schema, {required: true});
 console.log(res.valid);
