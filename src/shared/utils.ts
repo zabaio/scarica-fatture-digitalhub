2e0a0bc356dcb47819cd09720294e56d7876e0a5
Alberto Giusti <albertogiusti.giusti@gmail.com>
2026-02-13 14:57:49 +0100

    fix: commit deletion

--- src/shared/utils.ts
+++
@@ -1,9 +0,0 @@
-import dayjs from 'dayjs';
-
-//Utility formatting
-export function formatDMY(date: dayjs.Dayjs){ return date.format('DD-MM-YYYY'); }
-export function formatYMD(date: dayjs.Dayjs){ return date.format('YYYY-MM-DD'); }
-
-export function formatArchiveFilename(ces: string, start: dayjs.Dayjs, end: dayjs.Dayjs, cur: dayjs.Dayjs){
-  return "EXP_" + ces + "_" + formatYMD(start) + "_" + formatYMD(end) + "_" + cur.format('YYYY-MM-DD[T]HH-mm-ss') + ".zip";
-}
