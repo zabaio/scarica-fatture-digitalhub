8dc3de59c0e31a5293ee78b13451c5799b3bda71
Alberto Giusti <albertogiusti.giusti@gmail.com>
2026-02-14 07:19:16 +0100

    fix: workflow

--- src/remote.ts
+++ src/remote.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import * as path from 'path'
 import { main, configPath} from './main'
 import { loadConfig } from '@shared/schema-generator';
-const lastRunPath = path.join(process.cwd(), "config", "last-run.json");
+const lastRunPath = path.join(process.cwd(), "last-run.json");
 
 async function remoteStart (){
     const lastRun = JSON.parse(fs.readFileSync(lastRunPath, 'utf8'));
